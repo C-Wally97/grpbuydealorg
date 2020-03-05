6eeff8ba552a2e094505712443919d62745a22c8
Rob <robert.tootill98@gmail.com>
2020-03-05 12:07:40 +0000

    test api calls, in progress

--- client/index.js
+++ client/index.js
@@ -4,4 +4,15 @@ async function boot() {
   console.log("hi");
 }
 
+async function getProductListings(queryContent) {
+  let url = '/api/productListings';
+
+  if(queryContent) {
+    url = url + '?' + queryContent
+  }
+
+  const response = await fetch(url);
+  console.log(await response.json());
+}
+
 window.addEventListener("load", boot);
--- server.js
+++ server.js
@@ -3,8 +3,14 @@ const app = express();
 //const db = require('./ModelSQL.js');
 
 app.get('/api/productListings', getProductListings);
+app.get('/api/productListing', getProductListing);
 
 async function getProductListings(req, res) {
+    if(req.query) {
+      console.log("query info:" + req.query);
+    } else {
+      console.log("no query");
+    };
     res.json(productListings);
 }
 
@@ -15,7 +21,10 @@ app.use('/', express.static('client', {'extensions': ['html']}));
 let productListings = [
   {
     'id': 1,
-    'product': 'Bags',
-    'userAmount': 42
+    'product': 'Gamer shades',
+    'date': '01/02/2020',
+    'supplier': 'GamerSwag.com',
+    'userAmount': 42,
+    'rating': 420
   }
 ];
